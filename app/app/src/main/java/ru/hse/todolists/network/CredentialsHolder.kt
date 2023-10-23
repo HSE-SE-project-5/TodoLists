b60b2fd5f62eeecc490dcfbfcd291c3e62a1f2a3
MhlvDenis <dns.aqua.ban@gmail.com>
2023-10-23 19:44:39 +0300

    upload

---
+++ app/app/src/main/java/ru/hse/todolists/network/CredentialsHolder.kt
@@ -0,0 +1,13 @@
+package ru.hse.todolists.network
+
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+object CredentialsHolder {
+    var token: String? = null
+
+    val client =
+        OkHttpClient.Builder().pingInterval(1, TimeUnit.SECONDS).connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS).readTimeout(10, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(false).build()
+}
---
+++ app/app/src/main/java/ru/hse/todolists/other/Constants.kt
@@ -0,0 +1,11 @@
+package ru.hse.todolists.other
+
+object Constants {
+    const val HOST = "10.0.2.2"
+
+    const val PORT = 8080
+
+    const val SCHEME = "http"
+
+    const val BASE_URL = "$SCHEME://$HOST:$PORT/elysium/"
+}
